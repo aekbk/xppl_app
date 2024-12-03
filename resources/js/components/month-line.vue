16e050000f139a6ca61915f07b99e817636982f9
Yuan Lik Xun <yuan.likxun@bcg.com>
2024-12-03 12:02:53 +0800

    Add trendline compute method
    This monthline chart is a line graph that displays daily production
    volumes. We have also included a compute method to calculate the
    trendline using the given data: `this.data`.

--- resources/js/components/month-line.vue
+++ resources/js/components/month-line.vue
@@ -11,6 +11,7 @@
 
 <script>
 import VueApexCharts from "vue3-apexcharts";
+import { roundToDecimalPlace } from "../utils/number";
 
 export default {
     name: "MonthLine",
@@ -25,6 +26,30 @@ export default {
                     type: "line",
                     data: this.data,
                 },
+                {
+                    name: "TrendLine",
+                    type: "line",
+					// Compute trendline y = mx + b
+                    data: this.data.map((y, i, arr) => {
+                        const n = arr.length; // Total number of points
+                        const xValues = arr.map((_, idx) => idx); // Use indices as x-values
+
+                        // Compute the means of x and y
+                        const xMean = xValues.reduce((sum, x) => sum + x, 0) / n;
+                        const yMean = arr.reduce((sum, val) => sum + val, 0) / n;
+
+                        // Compute the slope (m) using the least squares formula
+                        const numerator = xValues.reduce((sum, x, k) => sum + (x - xMean) * (arr[k] - yMean), 0);
+                        const denominator = xValues.reduce((sum, x) => sum + (x - xMean) ** 2, 0);
+                        const slope = denominator !== 0 ? numerator / denominator : 0; // Avoid division by zero
+
+                        // Compute the intercept (b)
+                        const intercept = yMean - slope * xMean;
+
+                        // Compute the trendline value for the current index (x = i)
+                        return roundToDecimalPlace(slope * i + intercept);
+                    }),
+                }
             ];
         },
         chartOptions() {
